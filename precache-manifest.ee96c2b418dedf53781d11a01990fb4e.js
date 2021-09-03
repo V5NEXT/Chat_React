1edfd5b93924644d7c06d33e9a437bbb27908004
Vishnu Viswambharan <vviswambharan@DRGT.systems>
2021-09-03 09:53:53 +0200

    Updates

---
+++ precache-manifest.ee96c2b418dedf53781d11a01990fb4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3b3302bbfa8b32f250b13468b249d1",
+    "url": "/Chat_React/index.html"
+  },
+  {
+    "revision": "a52df437cb4fc88fea3d",
+    "url": "/Chat_React/static/css/2.5488a821.chunk.css"
+  },
+  {
+    "revision": "8adfd7f943053386627e",
+    "url": "/Chat_React/static/css/main.7b852056.chunk.css"
+  },
+  {
+    "revision": "a52df437cb4fc88fea3d",
+    "url": "/Chat_React/static/js/2.ed479eb8.chunk.js"
+  },
+  {
+    "revision": "26a4bd7f0725abe616ac15ecd1c64d1b",
+    "url": "/Chat_React/static/js/2.ed479eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adfd7f943053386627e",
+    "url": "/Chat_React/static/js/main.cd527607.chunk.js"
+  },
+  {
+    "revision": "159cde5cf1ad24a7b35e",
+    "url": "/Chat_React/static/js/runtime-main.db9bcd29.js"
+  },
+  {
+    "revision": "3db2481593f8762424b8b8d46fe1672b",
+    "url": "/Chat_React/static/media/logo.3db24815.png"
+  }
+]);
